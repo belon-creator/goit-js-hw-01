3b13bc5404bf126e241411b39e64267a9ef6b188
belon-creator <belon8911@gmail.com>
2020-04-16 22:23:43 +0300

    add two tasks

---
+++ task-01.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const name = 'Генератор защитного поля';
+let price = 1000;
+price = 2000;
+console.log('Выбран', name, 'цена за штуку', price);
+
+// const message = `Выбран ${name} цена за штуку ${price}`;
+// console.log(message);
---
+++ task-02.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const total = 100;
+const ordered = 50;
+if (ordered <= 100) {
+  console.log('Заказ оформлен, с вами свяжется менеджер');
+} else {
+  console.log('На складе недостаточно твоаров!');
+}
